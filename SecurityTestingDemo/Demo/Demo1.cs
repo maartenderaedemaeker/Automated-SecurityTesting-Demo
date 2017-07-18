5c1cedd2b529a28471528f8d29af36625c556974
Maarten De Raedemaeker <maartenderaedemaeker@outlook.com>
2017-07-18 12:09:47 +0200

    added class to demonstrate sonarqube rules

---
+++ SecurityTestingDemo/Demo/Demo1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SecurityTestingDemo.Demo
+{
+    /// <summary>
+    /// This class is used to demonstrate some SonarQube rules.
+    /// Enjoy the horror :-)
+    /// </summary>
+
+    public abstract class Demo1
+    {
+        public string async;
+        public static bool ShouldDispose = true;
+
+        //TODO: verify if constructor is needed
+        public Demo1()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            //FIXME: implement this properly
+            return null;
+        }
+
+        public void Dispose()
+        {
+            if (ShouldDispose == true)
+            {
+                //TODO: implement dispose
+            }
+            else if (ShouldDispose == true)
+            {
+                //TODO: implement dispose
+            }
+        }
+
+        public void Test(string a)
+        {
+            string b;
+
+            if (ShouldDispose)
+            {
+                goto DisposeMe;
+            } 
+
+            Console.WriteLine("This is a test");
+
+
+            DisposeMe: Dispose();
+        }
+    }
+}
