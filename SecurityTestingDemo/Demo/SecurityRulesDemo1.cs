b59646224c198ad93b1a5b3492801c4287243510
Maarten De Raedemaeker <maartenderaedemaeker@outlook.com>
2017-07-27 20:21:24 +0200

    got some security issues in

---
+++ SecurityTestingDemo/Demo/SecurityRulesDemo1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace SecurityTestingDemo.Demo
+{
+    public class SecurityRulesDemo1
+    {
+        public static string Username = "admin";
+        public static string Password = "hello123";
+
+        public static void EncryptNotSoSafely(string password)
+        {
+            
+            using (var tripleDes = new TripleDESCryptoServiceProvider())
+            {
+                // do something with provider
+            }
+        }
+
+        public static void InjectMe(string username)
+        {
+            const string password = "hello";
+            using (var con = new SqlConnection($"Data Source=localhost;Initial Catalog=Test;User id=admin;Password={password}"))
+            {
+                var sqlCmd = new SqlCommand("SELECT * FROM USERS WHERE Username='" + username + "'", con);
+                var reader = sqlCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0), reader.GetString(1));
+                }
+            }
+        }
+    }
+}
