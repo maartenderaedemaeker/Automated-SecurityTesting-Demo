ea2a5ff3958d882caeb7cc78cebb98783764a9ea
Maarten De Raedemaeker <maartenderaedemaeker@outlook.com>
2017-08-12 09:31:12 +0200

    CLEANUP

---
+++ InitializeDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Smo;
+namespace InitializeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["PetDB"].ConnectionString;
+            var sql = File.ReadAllText("Create.sql");
+            var conn = new SqlConnection(connectionString);
+            var server = new Server(new ServerConnection(conn));
+            server.ConnectionContext.ExecuteNonQuery(sql);
+
+            Console.WriteLine("DONE");
+            Console.ReadKey();
+        }
+    }
+}
--- SecurityTestingDemo/Demo/SecurityRulesDemo1.cs
+++ SecurityTestingDemo/Demo/SecurityRulesDemo1.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 
 namespace SecurityTestingDemo.Demo
 {
@@ -15,20 +13,6 @@ namespace SecurityTestingDemo.Demo
             using (var tripleDes = new TripleDESCryptoServiceProvider())
             {
                 // do something with provider
-            }
-        }
-
-        public static void InjectMe(string username)
-        {
-            const string password = "hello";
-            using (var con = new SqlConnection($"Data Source=localhost;Initial Catalog=Test;User id=admin;Password={password}"))
-            {
-                var sqlCmd = new SqlCommand("SELECT * FROM USERS WHERE Username='" + username + "'", con);
-                var reader = sqlCmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetString(0), reader.GetString(1));
-                }
             }
         }
     }
