81c119a8290bc7a3559a46ae4896de7cb152a239
Maarten De Raedemaeker <maartenderaedemaeker@outlook.com>
2017-09-09 14:31:10 +0200

    FEAT: evil dump

---
+++ Evil/Controllers/DumpController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Evil.Controllers
+{
+    [Route("api/[controller]")]
+    public class DumpController : Controller
+    {
+        public class Dump
+        {
+            public string Token { get; set; }
+            public string Url { get; set; }
+            public DateTime LogTime {get;set;}
+        }
+        private readonly IMemoryCache cache;
+        private const string CacheKey = "Dumps";
+        public DumpController(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+        [HttpGet("dump")]
+        public async Task DumpToken(string token, string url)
+        {
+            List<Dump> cachedDumps;
+            if(!cache.TryGetValue(CacheKey, out cachedDumps))
+            {
+                cachedDumps = new List<Dump>();
+            }
+
+            cachedDumps.Add(new Dump
+            {
+                Token = token,
+                Url = url,
+                LogTime = DateTime.Now
+            });
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(15));
+
+            cache.Set(CacheKey, cachedDumps);
+        }
+
+        [HttpGet("")]
+        public async Task<List<Dump>> GetDumps()
+        {
+            List<Dump> cachedDumps;
+            if(!cache.TryGetValue(CacheKey, out cachedDumps))
+            {
+                cachedDumps = new List<Dump>();
+            }
+
+            return cachedDumps;
+        }
+    }
+}
