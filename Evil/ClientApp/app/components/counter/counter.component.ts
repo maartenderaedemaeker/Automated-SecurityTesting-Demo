c223ac8f708bc4fb17ed37fb59058d2d5059f106
Maarten De Raedemaeker <maartenderaedemaeker@outlook.com>
2017-09-10 11:55:04 +0200

    display intercepted tokens

--- Evil/ClientApp/app/components/counter/counter.component.ts
+++ Evil/ClientApp/app/components/counter/counter.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import 'rxjs/Rx';
 
 @Component({
     selector: 'counter',
     templateUrl: './counter.component.html'
 })
-export class CounterComponent {
-    public currentCount = 0;
 
-    public incrementCounter() {
-        this.currentCount++;
+export class CounterComponent {
+
+    private dumps : object[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        console.log(baseUrl);
+        
+        IntervalObservable.create(5000).flatMap(() => {
+            return http.get(baseUrl + 'api/Dump');
+        })
+        .map((result) => {
+            return result.json();
+        })
+        .subscribe(result => {
+            this.dumps = result;
+        }, error => console.error(error));
     }
 }
